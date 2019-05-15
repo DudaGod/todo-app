f722cd4cb77571a6b1748a20d8207d6042d2264c
DudaGod <dudkevich@yandex-team.ru>
2019-05-15 23:19:29 +0300

    test: add integration tests using hermione

---
+++ .hermione.conf.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    baseUrl: 'http://localhost:3000',
+    gridUrl: 'http://localhost:4444/wd/hub',
+
+    browsers: {
+        chrome: {
+            desiredCapabilities: {
+                browserName: 'chrome'
+            }
+        }
+    },
+
+    plugins: {
+        'html-reporter/hermione': {
+            path: 'hermione-html-reporter'
+        }
+    }
+};
---
+++ test/integration/simple.hermione.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const {assert} = require('chai');
+
+describe('Todo-app', () => {
+    it('should add task to completed section', function() {
+        return this.browser
+            .url('/')
+            .click('.add-todo-text')
+            .keys('Hello world')
+            .click('.add-todo-btn')
+            .waitForVisible('.added-tasks input')
+            .click('.added-tasks input')
+            .isExisting('.completed-tasks li')
+            .then((exists) => {
+                assert.isTrue(exists, 'Todo item does not appear in completed section');
+            });
+    });
+
+    it('should render todo item correctly', function() {
+        return this.browser
+            .url('/')
+            .click('.add-todo-text')
+            .keys('Hello world')
+            .click('.add-todo-btn')
+            .waitForVisible('.added-tasks li')
+            .assertView('plain', '.added-tasks li');
+    });
+});
